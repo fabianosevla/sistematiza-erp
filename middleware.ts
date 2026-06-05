8e573f870ba3cd958180ba9dccd627687a3d2119
sevla02 <alves.fabianohenrique02@gmail.com>
2026-06-05 19:29:32 -0300

    fix: clerk middleware syntax v5

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/webhooks(.*)',
+  '/onboarding(.*)',
 ])
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect()
+    await auth.protect()
   }
-  return NextResponse.next()
 })
 
 export const config = {
